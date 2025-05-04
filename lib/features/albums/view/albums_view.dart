14a084c7b9754d422cbd36ae1b0b9ada5b477da6
toanlemanh <chen22112k3@gmail.com>
2025-05-04 21:46:02 +0700

    create album view

---
+++ lib/features/albums/view/albums_view.dart
@@ -0,0 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx_album_app/features/albums/model/album.dart';
+import 'package:mobx_album_app/features/albums/model/service_path.dart';
+import 'package:mobx_album_app/features/albums/service/albums_service.dart';
+import 'package:mobx_album_app/features/albums/service/service_path_extension.dart';
+import 'package:mobx_album_app/features/albums/viewmodel/albums_view_model.dart';
+import 'package:mobx_album_app/widgets/album_card.dart';
+
+class AlbumsView extends StatelessWidget {
+  AlbumsView({super.key});
+
+  final _albumsViewModel = AlbumsViewModel(
+    albumService: AlbumsService(
+      Dio(BaseOptions(baseUrl: ServicePath.BASEURL.rawValue)),
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Observer(builder: (context) =>
+    _albumsViewModel.isLoading ? CircularProgressIndicator(backgroundColor: Colors.red,)
+        : ListView.builder(
+          itemCount: _albumsViewModel.albumCount,
+          itemBuilder: (context, index){
+            return AlbumCard(model: _albumsViewModel.albumList[index]);
+          },
+        )
+    );
+  }
+}
